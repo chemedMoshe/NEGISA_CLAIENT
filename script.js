c1364a003f73d53185a5faf591d5d43fee138d38
Moshe <m0504136536@gmail.com>
2024-10-20 02:25:51 +0300

    btn users done

--- script.js
+++ script.js
@@ -10,8 +10,10 @@ const createElements = (element,content = "") => {
     return newElement;
 }
 
-creditBtn.addEventListener("click", creditsBtn)
-function creditsBtn() {
+creditBtn.addEventListener("click", creditsBtnFunc)
+usersBtn.addEventListener("click", usersBtnFunc)
+
+function creditsBtnFunc() {
    const h1 = createElements("h1","זיכויים");
     const addCreditBtn = createElements("button","הוספת זיכוי");
     const creditPerUserBtn = createElements("button","זיכויים לפי משתמש"); 
@@ -21,9 +23,19 @@ function creditsBtn() {
     contentByJs.appendChild(creditPerUserBtn);
     contentByJs.appendChild(allCreditBtn);
     contentByJs.style.display = "flex";
-   
-    
-
 }
+
+
+function usersBtnFunc() {
+    const h1 = createElements("h1","משתמשים");
+     const addCreditBtn = createElements("button","הוספת משתמש");
+     const creditPerUserBtn = createElements("button","בדיקת יתרת משתמש"); 
+     const allCreditBtn = createElements("button","כל המשתמשים");
+     contentByJs.appendChild(h1);
+     contentByJs.appendChild(addCreditBtn);
+     contentByJs.appendChild(creditPerUserBtn);
+     contentByJs.appendChild(allCreditBtn);
+     contentByJs.style.display = "flex";
+ }
 
 
