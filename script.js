6a04c8f0e3a491296be737bf7eaaad6a7a4822d9
Moshe <m0504136536@gmail.com>
2024-10-20 22:51:55 +0300

    add deletetn

--- script.js
+++ script.js
@@ -19,10 +19,13 @@ function creditsBtnFunc() {
     const addCreditBtn = createElements("button", "הוספת זיכוי");
     const creditPerUserBtn = createElements("button", "זיכויים לפי סועד");
     const allCreditBtn = createElements("button", "כל הזיכויים");
+    const deleteCreditBtn = createElements("button", "מחיקת זיכוי");
+   
     contentByJs.appendChild(h1);
     contentByJs.appendChild(addCreditBtn);
     contentByJs.appendChild(creditPerUserBtn);
     contentByJs.appendChild(allCreditBtn);
+    contentByJs.appendChild(deleteCreditBtn);
     contentByJs.style.display = "flex";
 }
 
@@ -33,10 +36,12 @@ function usersBtnFunc() {
     const addCreditBtn = createElements("button", "הוספת סועד");
     const creditPerUserBtn = createElements("button", "בדיקת יתרת סועד");
     const allCreditBtn = createElements("button", "כל הסועדים");
+    const deleteCreditBtn = createElements("button", "מחיקת סועד");
     contentByJs.appendChild(h1);
     contentByJs.appendChild(addCreditBtn);
     contentByJs.appendChild(creditPerUserBtn);
     contentByJs.appendChild(allCreditBtn);
+    contentByJs.appendChild(deleteCreditBtn);
     contentByJs.style.display = "flex";
     contentByJs.style.position = "relative";
 }
