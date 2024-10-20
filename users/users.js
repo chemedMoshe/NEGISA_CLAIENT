cc19e0e28d31cf0b0912ec43ee82d187776b8fed
Moshe <m0504136536@gmail.com>
2024-10-20 23:56:10 +0300

    addcreate users func

--- users/users.js
+++ users/users.js
@@ -2,18 +2,34 @@ import { createElements } from "../script.js";
 import { contentByJs } from "../script.js";
 export function usersBtnFunc() {
     contentByJs.innerHTML = "";
-    const h1 = createElements("h1", "סועדים");
-    const addCreditBtn = createElements("button", "הוספת סועד");
+    const addUseritBtn = createElements("button", "הוספת סועד");
     const creditPerUserBtn = createElements("button", "בדיקת יתרת סועד");
     const deleteCreditBtn = createElements("button", "מחיקת סועד");
     const editCreditBtn = createElements("button", "עריכת סועד");
     const allCreditBtn = createElements("button", "כל הסועדים");
-    contentByJs.appendChild(h1);
-    contentByJs.appendChild(addCreditBtn);
+    addUseritBtn.addEventListener("pointerdown", createUsers);
+    
+    contentByJs.appendChild(createElements("h1", "סועדים"));
+    contentByJs.appendChild(addUseritBtn);
     contentByJs.appendChild(editCreditBtn);
     contentByJs.appendChild(creditPerUserBtn);
     contentByJs.appendChild(allCreditBtn);
     contentByJs.appendChild(deleteCreditBtn);
     contentByJs.style.display = "flex";
     contentByJs.style.position = "absolute";
+}
+
+function createUsers(){
+    contentByJs.innerHTML = "";
+    contentByJs.appendChild(createElements("h1", "הוספת סועד"));
+    const inputUserName = createElements("input");
+    inputUserName.setAttribute("placeholder", "שם הסועד");
+    const inputUserPhone = createElements("input");
+    inputUserPhone.setAttribute("placeholder", "טלפון הסועד");
+    const sumcredit = createElements("input");
+    sumcredit.setAttribute("placeholder", "יתרת זכות");
+    contentByJs.appendChild(inputUserName);
+    contentByJs.appendChild(inputUserPhone);
+    contentByJs.appendChild(sumcredit);
+    
 }
