a9490f0456a45485557efd6156d681c326c968ec
Moshe <m0504136536@gmail.com>
2024-10-28 17:41:21 +0200

    https://negisaserver-1.onrender.com

--- fetchs/fetchLayer.js
+++ fetchs/fetchLayer.js
@@ -15,7 +15,7 @@ export const createNewUserFetch = async (user) => {
 
 export const getAllUsersFetch = async () => {
     try {
-        const response = await fetch("https://negisaserver-12.onrender.com/user/");
+        const response = await fetch("https://negisaserver-1.onrender.com/user/");
         const data = await response.json();
         return data;
     } catch (error) {
@@ -26,7 +26,7 @@ export const getAllUsersFetch = async () => {
 export const deleteUserFetch = async (user) => {
     try {
 
-        const response = await fetch(`https://negisaserver-12.onrender.com/user/`, {
+        const response = await fetch(`https://negisaserver-1.onrender.com/user/`, {
             method: "DELETE",
             headers: {
                 "Content-Type": "application/json",
@@ -41,37 +41,36 @@ export const deleteUserFetch = async (user) => {
 }
 
 export const updateUserFetch = async (id, userUpdated) => {
-    try{
-    const response = await fetch(`https://negisaserver-12.onrender.com/user/${id}`, {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(userUpdated),
-    })
+    try {
+        const response = await fetch(`https://negisaserver-1.onrender.com/user/${id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(userUpdated),
+        })
 
         const data = await response.json()
         return data
     }
-    catch(error){
+    catch (error) {
         return error
     }
 }
 
 export const searchUserFetch = async (username) => {
-    try{
-    const response = await fetch(`https://negisaserver-12.onrender.com/user/search`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(username),
-    })
-    const data = await response.json()
-    return data.result
+    try {
+        const response = await fetch(`https://negisaserver-1.onrender.com/user/search`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(username),
+        })
+        const data = await response.json()
+        return data.result
     }
-    catch(error){
+    catch (error) {
         return error
     }
 }
-    
